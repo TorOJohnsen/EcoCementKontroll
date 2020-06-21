c0f4864c5b3c4d4aaeeb0b2ef606cf7abb99d8aa
Tor Oskar Ova Johnsen <tor_johnsen@hotmail.com>
2020-06-21 15:40:27 +0200

    Config helpers

---
+++ CementControl/CementControl.Tests/SerialPortConfigTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CementControl;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CementControl.Tests
+{
+    [TestClass()]
+    public class SerialPortConfigTests
+    {
+
+
+        private readonly string _connString1 = "9600,None,One,8,None,\n";
+        private readonly string _connString2 = "9600,None,One,8,None,\r";
+
+
+        [TestMethod()]
+        public void GetBaudRateTest()
+        {
+            var cfg = new SerialPortConfig(_connString1);
+            cfg.GetBaudRate().Should().Be(9600);
+        }
+
+        [TestMethod()]
+        public void GetParityTest()
+        {
+            var cfg = new SerialPortConfig(_connString2);
+            cfg.GetParity().Should().Be(Parity.None);
+        }
+
+        [TestMethod()]
+        public void GetStopBitsTest()
+        {
+            var cfg = new SerialPortConfig(_connString2);
+            cfg.GetStopBits().Should().Be(StopBits.One);
+        }
+
+        [TestMethod()]
+        public void GetDataBitsTest()
+        {
+            var cfg = new SerialPortConfig(_connString2);
+            cfg.GetDataBits().Should().Be(8);
+        }
+
+        [TestMethod()]
+        public void GetHandshakeTest()
+        {
+            var cfg = new SerialPortConfig(_connString2);
+            cfg.GetHandshake().Should().Be(Handshake.None);
+        }
+
+        [TestMethod()]
+        public void GetNewLineTest()
+        {
+            var cfg = new SerialPortConfig(_connString1);
+            cfg.GetNewLine().Should().Be("\n");
+        }
+    }
+}
---
+++ CementControl/CementControl/SerialPortConfig.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CementControl
+{
+    public class SerialPortConfig
+    {
+
+        //(_powerSupplyComPort, 9600, Parity.None, StopBits.One, 8, Handshake.None, "\n");
+        // <add key = "app:pwrSypplySerialConfig" value="9600,None,One,8,None,\n" />
+        private readonly int _baudRate;
+        private readonly Parity _parity;
+        private readonly StopBits _stopBits;
+        private readonly int _dataBits;
+        private readonly Handshake _handshake;
+        private readonly string _newLine;
+
+
+        private readonly string _connectionSting;
+
+        public SerialPortConfig(string connectionString)
+        {
+            _connectionSting = connectionString;
+
+
+            List<string> connString = SplitList();
+            _baudRate = Convert.ToInt32(connString[0]);
+            Enum.TryParse(connString[1], out _parity);
+            Enum.TryParse(connString[2], out _stopBits);
+            _dataBits = Convert.ToInt32(connString[3]);
+            Enum.TryParse(connString[4], out _handshake);
+            _newLine = connString[5];
+
+
+
+        }
+
+
+        private List<string> SplitList()
+        {
+            return _connectionSting.Split(',').ToList();
+        }
+
+        public int GetBaudRate()
+        {
+            return _baudRate;
+        }
+
+        public Parity GetParity()
+        {
+            return _parity;
+        }
+
+        public StopBits GetStopBits()
+        {
+            return _stopBits;
+        }
+
+        public int GetDataBits()
+        {
+            return _dataBits;
+        }
+
+        public Handshake GetHandshake()
+        {
+            return _handshake;
+        }
+
+        public string GetNewLine()
+        {
+            return _newLine;
+        }
+
+    }
+
+
+}
