95af957ab704616a8184ac29af274fc62e292e1e
Tor Oskar Ova Johnsen <tor_johnsen@hotmail.com>
2020-06-28 01:58:49 +0200

    Starting to remove database

---
+++ CementControl/CementControl/DataAccess/DataFileHandle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace CementControl.DataAccess
+{
+    public class DataFileHandle
+    {
+        private readonly ILogger _logger = Log.ForContext<DataFileHandle>();
+
+
+
+        public DataFileHandle()
+        {
+
+        }
+
+
+    }
+}
