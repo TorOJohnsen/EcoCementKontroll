53cc12fce093412393f699ca2aeefe7646ad6260
Tor Oskar Ova Johnsen <tor_johnsen@hotmail.com>
2020-07-03 23:40:06 +0200

    Added mutex to only run one instance

--- CementControl/CementControl/Program.cs
+++ CementControl/CementControl/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,31 @@ namespace CementControl
 {
     static class Program
     {
+
+        static Mutex mutex = new Mutex(true, "{ac1303ff-1e86-4593-8bcb-c2f89cdc12b3}");
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                try
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainWindow());
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Programmet kjører allerede i et annet vindu");
+            }   
         }
     }
 }
